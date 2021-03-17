d6559c3c0c9ca98439bb70afa071dba59cc418f0
risitasfinance <risitasfinance@protonmail.com>
2021-03-17 21:28:32 +0100

    Commit

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2755,7 +2755,7 @@ var baseColors = {
 var brandColors = {
     binance: "#972e55",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#ce4073", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#222222", textDisabled: "#BDC2C4", textSubtle: "#8f80ba", borderColor: "#E9EAEB", card: "#ce4073", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#ce4073", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#222222", textDisabled: "#BDC2C4", textSubtle: "#ffffff", borderColor: "#E9EAEB", card: "#ce4073", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#222222", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#ce4073", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -25,7 +25,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#222222",
   textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  textSubtle: "#ffffff",
   borderColor: "#E9EAEB",
   card: "#ce4073",
   gradients: {
